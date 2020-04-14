66dc0a7f0651a4c5cd307e34eac750eed8a10ce3
dev1980 <brham1980@gmail.com>
2020-04-14 19:44:44 +0545

    implement operate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,11 +1,29 @@
-import Big from 'big-js';
 import operate from './operate';
 
 const calculate = (data, name) => {
-    if(name === 'AC' || name === '.' || name === '=') {
+    if(name === 'AC') {
         return {
             total = null,
             next = null,
             operation = null,
         };
     }
+    let result;
+    const { total, next, operation } = data;
+    switch (name) {
+        case '+/-':
+          result = total * (-1);
+          break;
+        case '.':
+          result = '.';
+          break;
+        case '=':
+          result = operate(total, next, operation);
+          break;
+        default:
+      }
+    
+      return result;
+    };
+
+    export default calculate;
