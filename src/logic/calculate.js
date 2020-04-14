a5a972597ab0847ec24790a607b6ac320eb1e502
dev1980 <brham1980@gmail.com>
2020-04-14 19:31:23 +0545

    crate calculate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,11 @@
 import Big from 'big-js';
 import operate from './operate';
 
+const calculate = (data, name) => {
+    if(name === 'AC' || name === '.' || name === '=') {
+        return {
+            total = null,
+            next = null,
+            operation = null,
+        };
+    }
