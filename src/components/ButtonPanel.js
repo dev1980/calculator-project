5af2e802478f680378a2c7023c022848afe5559a
dev1980 <brham1980@gmail.com>
2020-04-17 05:19:30 +0545

    fixed the linter error

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = ({clickHandler}) => {
+const ButtonPanel = ({ clickHandler }) => {
   const handleClick = buttonName => {
     clickHandler(buttonName);
   };
